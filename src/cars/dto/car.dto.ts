6a7524097601a72b64aa81fe08031738e5f825f0
Adrian Dumitrache <dumitrache.ady91@gmail.com>
2022-05-29 23:51:45 +0300

    Updated docs.

--- src/cars/dto/car.dto.ts
+++ src/cars/dto/car.dto.ts
@@ -9,24 +9,44 @@ import {
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CarDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Make',
+    required: true,
+    example: 'Audi',
+  })
   @IsString()
   @IsNotEmpty()
   readonly make!: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Model',
+    required: true,
+    example: 'A4',
+  })
   @IsString()
   @IsNotEmpty()
   readonly model!: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Year',
+    required: true,
+    minimum: 1900,
+    maximum: 2022,
+    example: 2014,
+  })
   @IsInt()
   @IsNotEmpty()
   @Min(1900)
   @Max(2022)
   readonly year!: number;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({
+    description: 'Mileage',
+    required: false,
+    minimum: 0,
+    example: 100000,
+    default: null,
+  })
   @IsInt()
   @IsOptional()
   @Min(0)
